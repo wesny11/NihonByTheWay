99ec39dbb27c469a7497956c63ed65df356686a7
blazstempelj <blaz.stempelj@gmail.com>
2014-05-07 10:12:13 +0200

    Index je narejen.

--- index.php
+++ index.php
@@ -24,12 +24,35 @@
 	</header>
 
 	<div class="main-content">
+		<div class="slides-container center">
+			<div id="slides">
+				<img src="images/slideshow/slide1-crop.jpg" />
+				<img src="images/slideshow/slide2-crop.jpg" />
+				<img src="images/slideshow/slide3-crop.jpg" />
+			</div>
+		</div>
 		<div class="row">
-			<div class="slides-container center">
-				<div id="slides">
-					<img src="images/slideshow/slide3.jpg" />
-					<img src="images/slideshow/slide4.jpg" />
-					<img src="images/slideshow/slide7.jpg" />
+			<div class="index-content">
+				<div class="even">
+					<div class="text">
+						<h3>Spoznajte sushi na <span class="red">spletu</span>.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quos, quisquam, sunt aliquid voluptatem eum aut nesciunt consequatur aspernatur unde recusandae facilis neque ipsam ipsum iste voluptatum culpa incidunt voluptatibus.</p>
+					</div>					
+					<img src="images/Inari.JPG" alt="">
+				</div>
+				<div class="odd clearfix">
+					<div class="text">
+						<h3>Enostavno naročanje in hitra dostava.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, expedita dolorum perferendis incidunt blanditiis molestias laborum asperiores! Molestias, fuga vitae earum quae suscipit modi dolor nulla tenetur impedit laborum hic.</p>
+					</div>
+					<img src="images/Norimaki.jpg" alt="">				
+				</div>
+				<div class="even clearfix">
+					<div class="text">
+						<h3>Svežina in kakovost.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, debitis, tempora, quibusdam, ipsam iste sunt quae necessitatibus autem molestiae pariatur quod porro nihil magnam molestias possimus reprehenderit repellendus laboriosam iure.</p>
+					</div>
+					<img src="images/Tonkatsu.jpg" alt="">
 				</div>
 			</div>
 		</div>	
@@ -49,9 +72,7 @@
 				play: {
 					active: false,
 					interval: 5000,
-					auto: true,
-					pauseOnHover: true,
-					restartDelay: 2500
+					auto: true
 				}
 			});
 		});
